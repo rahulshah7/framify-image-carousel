809dde4b49101a102a375db5125cf16ac6b91664
Rahul Shah <rahulshah7@outlook.com>
2019-08-16 07:48:41 +0100

    Wire up prev and next buttons

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const framifyElements = document.querySelectorAll(".framify-element");
-console.log(framifyElements);
-
+const container = document.querySelector(".framify-container");
+const nextButton = document.querySelector(".framify-button-next");
+const previousButton = document.querySelector(".framify-button-previous");
 const framifyElementsWrapper = document.querySelector(
   ".framify-elements-wrapper"
 );
+const framifyElements = document.querySelectorAll(".framify-element");
+
+const width = container.clientWidth;
+const scrollSpeed = parseInt(container.getAttribute("data-scroll-speed"));
+
+/* Event Listeners */
+
+nextButton.addEventListener("click", () =>
+  scrollElement(framifyElementsWrapper, scrollSpeed, width)
+);
+
+previousButton.addEventListener("click", () =>
+  scrollElement(framifyElementsWrapper, -scrollSpeed, width)
+);
+
+/* Event Handlers */
 
 function scrollElement(elementToScroll, scrollVelocity, scrollMagnitude) {
   let scrolled = 0;
